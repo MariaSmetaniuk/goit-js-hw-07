ac6d43935509195a8c3f4cf5abb7b6474f6f6a28
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-07-18 15:39:52 +0300

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// посилання на div з галереєю
+const galleryRef = document.querySelector('.gallery');
+
+// створення і рендер розмітки елементів галереї
+const galleryElements = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item"><a href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"></a></li>`).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', galleryElements);
+
+
+// бібліотека SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250' });
